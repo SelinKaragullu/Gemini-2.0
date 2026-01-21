86eae95dea5432f6294bb231dfc71c2e9a8d3a09
Selin Karagullu <selin.karagullu@gmail.com>
2026-01-21 22:00:41 +0300

    Update practise.jsx

---
+++ src/practise.jsx
@@ -0,0 +1,32 @@
+const response = await fetch ()
+const data = await response.json()
+
+console.log(data)
+
+fetch()
+.then(response=>response.json())
+.then(data=>console.log(data))
+.catch(err=>console.log(err))
+
+async function getSug () {
+  const response = await fetch()
+  const data = await response.json()
+}
+
+getSug()
+
+
+useEffect(()=>{
+const getData = async ()=>{
+try{ const response = await fetch ()
+  const data = await response.json()
+
+}catch(error{
+console.log(error)
+throw new Error ("")
+}
+}
+
+getData()
+
+},[])
